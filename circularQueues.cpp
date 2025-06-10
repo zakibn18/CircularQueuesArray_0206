223c56b025e6c19cc5a4e966dce17b3f9fb973e0
zakibn18 <jekben815@gmail.com>
2025-06-10 11:11:24 +0700

    iterasi dari awal array hingga REAR

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -103,6 +103,16 @@ public:
         cout << queue_array[FRONT_position] << " ";
         FRONT_position++;
       }
+      
+      FRONT_position = 0;
+
+      // iterasi dari awal array hingga REAR
+      while (FRONT_position <= REAR_position)
+      {
+        cout << queue_array[FRONT_position] << " ";
+        FRONT_position++;
+      }
+      cout << endl;
     }
   }
 };
