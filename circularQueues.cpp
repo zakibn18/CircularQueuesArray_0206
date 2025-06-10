ef206eaa44c705ade1237095307a166debe9b516
zakibn18 <jekben815@gmail.com>
2025-06-10 10:45:56 +0700

    jika REAR berada di posisi terakhir array, kembali ke awal array

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -33,7 +33,18 @@ public:
       FRONT = 0;
       REAR = 0;
     }
+    else
+    {
+      // jika REAR berada di posisi terakhir array, kembali ke awal array
+      if (REAR == max - 1)
+        REAR = 0;
+      else
+        REAR = REAR + 1;
+    }
+    queue_array[REAR] = num;
   }
+
+  
 };
 
 int main()
