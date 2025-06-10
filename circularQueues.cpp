bc6468d4435b852c39bbe0486f0c4d1b32fbf129
zakibn18 <jekben815@gmail.com>
2025-06-10 10:55:19 +0700

    membuat methode

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -46,7 +46,28 @@ public:
 
   void remove()
   {
-    
+    // cek apakah antrian kosong
+    if (FRONT == - 1)
+    {
+      cout << "Queue underflow\n";
+      return;
+    }
+    cout << "\nThe element deleted from the queue is : " << queue_array[FRONT] << "\n";
+
+    // cek jika antrian hanya memiliki satu elemen
+    if (FRONT == REAR)
+    {
+      FRONT = -1;
+      REAR = -1;
+    }
+    else
+    {
+      // jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+      if (FRONT == max -1)
+        FRONT = 0;
+      else
+        FRONT = FRONT + 1; 
+    }
   }
 };
 
