57c0706de136ad707e80c0d3a597415401cabd12
zakibn18 <jekben815@gmail.com>
2025-06-10 10:40:22 +0700

    cek apakah antrian penuh

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -19,6 +19,13 @@ public:
     cout << "Enter a number : ";
     cin >> num;
     cout << endl;
+
+    // cek apakah antrian penuh
+    if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+    {
+      cout << "\nQueue overflow\n";
+      return;
+    }
   }
 };
 
